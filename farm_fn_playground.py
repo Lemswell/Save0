da3655d694ecf5470c55a7729316cc6c2d34dbe6
Lemuel <42715753+Lemswell@users.noreply.github.com>
2025-10-22 15:47:26 +1100

    edits to experimental

--- farm_fn_playground.py
+++ farm_fn_playground.py
@@ -1,7 +1,7 @@
 import setup_fn
 import g_fn
 
-def wcg_farm(x_from, x_to):
+def wcg_farm(x_from, x_to, y_from, y_to):
 	g_fn.go_to_pos(x_from, y_from)
 	
 	for i in range(x_from, x_to):
@@ -24,9 +24,9 @@ def wcg_farm(x_from, x_to):
 		move(East)
 		
 
-def pumpkin_farm(x_from, x_to):
-	while True:
-		for i in range(get_world_size()):
+def pumpkin_farm(x_from, x_to, y_from, y_to):
+	for i in range(x_from, x_to):
+		for y in range(y_from, y_to):
 			if can_harvest(): 
 				harvest()
 			else:
@@ -34,10 +34,7 @@ def pumpkin_farm(x_from, x_to):
 				while not can_harvest():
 					continue
 				harvest()
-			if get_ground_type() == Grounds.Soil:
-				plant(Entities.Carrot)
-			if g_fn.is_even(get_pos_y() + get_pos_x()):
-				plant(Entities.Tree)
+			plant(Entities.Pumpkin)
 			move(North)
 		move(East)
 		
